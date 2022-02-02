ef7c266d3c767f17877699942222e0aeeb893f05
RabeloGuedes <krepscredian@gmail.com>
2022-02-02 15:55:49 -0300

    Adiciona o bloco 4.

---
+++ Fudamentos/bloco4-js-introduction/dia01/scripts.js
@@ -0,0 +1,11 @@
+const myName = 'Alê';
+const birthCity = 'Ilhéus';
+let birthYear = 1997;
+
+console.log(myName, birthCity, birthYear);
+
+birthYear = 2030;
+console.log(birthYear);
+
+birthCity = 'Salvador';
+console.log(birthCity);
---
+++ Fudamentos/bloco4-js-introduction/scripts.js
@@ -0,0 +1,16 @@
+// const myName = 'Alê';
+// const birthCity = 'Ilhéus';
+// let birthYear = 1997;
+
+// console.log(myName, birthCity, birthYear);
+
+// birthYear = 2030;
+// console.log(birthYear);
+
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
