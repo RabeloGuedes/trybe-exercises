74e49fbaa30957001f491af53eacb3f53f623834
RabeloGuedes <krepscredian@gmail.com>
2022-02-02 18:19:23 -0300

    Adiciona o bloco 4.

--- Fudamentos/bloco4-js-introduction/scripts.js
+++ Fudamentos/bloco4-js-introduction/scripts.js
@@ -7,10 +7,18 @@
 // birthYear = 2030;
 // console.log(birthYear);
 
-let patientId = 50;
-let isEnrolled = true;
-const patientInfo = {
-  firstName: 'Ana',
-  lastName: 'Santos',
-};
-const patientEmail = 'ana@email.com';
+// let patientId = '50';
+// let isEnrolled = true;
+// const patientInfo = {
+//   firstName: 'Ana',
+//   lastName: 'Santos',
+// };
+// const patientEmail = 'ana@email.com';
+
+const base = 5;
+const height = 8;
+
+const area = base * height;
+const perimeter = 2*base + 2*height;
+
+console.log(area, perimeter);
