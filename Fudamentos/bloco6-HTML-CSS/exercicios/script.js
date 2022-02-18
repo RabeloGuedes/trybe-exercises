ba5c14fd0edc3d082db7a49f40239757d8f6fd0b
RabeloGuedes <krepscredian@gmail.com>
2022-02-18 13:54:34 -0300

    Adiciona exercicios.

---
+++ Fudamentos/bloco6-HTML-CSS/exercicios/script.js
@@ -0,0 +1,6 @@
+const sendBtn = document.querySelector('#send-btn');
+const clearBtn = document.querySelector('#clear-btn');
+const allInputs = document.querySelectorAll('input');
+sendBtn.addEventListener('click', function (event) {
+  event.preventDefault();
+});
