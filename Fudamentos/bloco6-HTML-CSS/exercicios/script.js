76ebaa6b3b0898666e7ee22fd966f7870b249be6
RabeloGuedes <krepscredian@gmail.com>
2022-02-21 11:35:41 -0300

    Adiciona o dia 2.

--- Fudamentos/bloco6-HTML-CSS/exercicios/script.js
+++
@@ -1,6 +0,0 @@
-const sendBtn = document.querySelector('#send-btn');
-const clearBtn = document.querySelector('#clear-btn');
-const allInputs = document.querySelectorAll('input');
-sendBtn.addEventListener('click', function (event) {
-  event.preventDefault();
-});
