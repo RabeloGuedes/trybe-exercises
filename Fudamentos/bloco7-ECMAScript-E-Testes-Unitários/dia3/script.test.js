15ef2695314820007f4d03d6d4db18aa323979cf
RabeloGuedes <krepscredian@gmail.com>
2022-03-07 19:05:26 -0300

    Adiciona o exercicio I.

---
+++ Fudamentos/bloco7-ECMAScript-E-Testes-Unitários/dia3/script.test.js
@@ -0,0 +1,10 @@
+const sum = require('./script.js');
+
+describe('Requisito 1', () => {
+  test('Sums two numbers.', () => {
+    expect(sum(2, 3)).toBe(5);
+  });
+  test('Try to sum a number and a string.', () => {
+    expect(() => sum(4, '5')).toThrowError('parameters must be numbers');
+  })
+});
