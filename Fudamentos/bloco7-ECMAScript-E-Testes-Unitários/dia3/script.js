ed6c7149720fc948791c49100b0efeda4ff46c3e
RabeloGuedes <krepscredian@gmail.com>
2022-03-07 19:19:18 -0300

    Adiciona teste do requisito 2.

--- Fudamentos/bloco7-ECMAScript-E-Testes-Unitários/dia3/script.js
+++ Fudamentos/bloco7-ECMAScript-E-Testes-Unitários/dia3/script.js
@@ -7,3 +7,26 @@ function sum(a, b) {
 }
 
 module.exports = sum;
+
+
+function myRemove(arr, item) {
+  let newArr = [];
+  for (let index = 0; index < arr.length; index += 1) {
+    if (item !== arr[index]) {
+      newArr.push(arr[index]);
+    }
+  }
+  return newArr;
+}
+
+describe('Requisito 2', () => {
+  test('Remove an item from an array', () => {
+    expect(myRemove([1, 2, 3, 4], 3)).not.toContain(3);
+  });
+  test(`Not return the array [1, 2, 3, 4]`, () => {
+    expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
+  });
+  test('Return the exepected array [1, 2, 3, 4]', () => {
+    expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
+  });
+});
