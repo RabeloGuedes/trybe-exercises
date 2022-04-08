9ef339ac9d42401fad6da334c01722e3e290f9d5
RabeloGuedes <krepscredian@gmail.com>
2022-04-08 10:28:30 -0300

    Adiciona o diretorio de FrontEnd

--- frontEnd/bloco-10-introducao-a-react/dia-1-hello-world-no-react/hello-world/src/App.js
+++ frontEnd/bloco-10-introducao-a-react/dia-1-hello-world-no-react/hello-world/src/App.js
@@ -1,6 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 
+const Task = (value) => {
+  return (
+    <li>{value}</li>
+  );
+}
+
 function App() {
   return (
     <div className="App">
@@ -9,6 +15,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Task value = 'Olá' />
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ frontEnd/bloco-10-introducao-a-react/fixation-exercises-10-2/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Image from './Image';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={Image} className="App-logo" alt="Cute cat staring" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
